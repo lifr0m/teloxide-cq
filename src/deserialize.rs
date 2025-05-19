1b819c77cfcf57b57248f9384388c7da618b21b1
Lifr0m <artem.kay@yandex.ru>
2025-05-19 12:59:01 +0300

    add types: u8, Vec<u8>, String

--- src/deserialize.rs
+++ src/deserialize.rs
@@ -1,13 +1,32 @@
 use rust_decimal::Decimal;
 use std::io::{Cursor, Read};
+use std::string::FromUtf8Error;
+use thiserror::Error;
 use uuid::Uuid;
 
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("io: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("string decode: {0}")]
+    StringDecode(#[from] FromUtf8Error),
+}
+
 pub trait Deserialize: Sized {
-    fn deserialize(src: &mut Cursor<Vec<u8>>) -> std::io::Result<Self>;
+    fn deserialize(src: &mut Cursor<Vec<u8>>) -> Result<Self, Error>;
+}
+
+impl Deserialize for u8 {
+    fn deserialize(src: &mut Cursor<Vec<u8>>) -> Result<Self, Error> {
+        let mut buf = [0; 1];
+        src.read_exact(&mut buf)?;
+        Ok(Self::from_le_bytes(buf))
+    }
 }
 
 impl Deserialize for i64 {
-    fn deserialize(src: &mut Cursor<Vec<u8>>) -> std::io::Result<Self> {
+    fn deserialize(src: &mut Cursor<Vec<u8>>) -> Result<Self, Error> {
         let mut buf = [0; 8];
         src.read_exact(&mut buf)?;
         Ok(Self::from_le_bytes(buf))
@@ -15,7 +34,7 @@ impl Deserialize for i64 {
 }
 
 impl Deserialize for u64 {
-    fn deserialize(src: &mut Cursor<Vec<u8>>) -> std::io::Result<Self> {
+    fn deserialize(src: &mut Cursor<Vec<u8>>) -> Result<Self, Error> {
         let mut buf = [0; 8];
         src.read_exact(&mut buf)?;
         Ok(Self::from_le_bytes(buf))
@@ -23,7 +42,7 @@ impl Deserialize for u64 {
 }
 
 impl Deserialize for Uuid {
-    fn deserialize(src: &mut Cursor<Vec<u8>>) -> std::io::Result<Self> {
+    fn deserialize(src: &mut Cursor<Vec<u8>>) -> Result<Self, Error> {
         let mut buf = [0; 16];
         src.read_exact(&mut buf)?;
         Ok(Self::from_bytes_le(buf))
@@ -31,9 +50,23 @@ impl Deserialize for Uuid {
 }
 
 impl Deserialize for Decimal {
-    fn deserialize(src: &mut Cursor<Vec<u8>>) -> std::io::Result<Self> {
+    fn deserialize(src: &mut Cursor<Vec<u8>>) -> Result<Self, Error> {
         let mut buf = [0; 16];
         src.read_exact(&mut buf)?;
         Ok(Self::deserialize(buf))
+    }
+}
+
+impl<T: Deserialize> Deserialize for Vec<T> {
+    fn deserialize(src: &mut Cursor<Vec<u8>>) -> Result<Self, Error> {
+        let len = u8::deserialize(src)? as usize;
+        (0..len).map(|_| T::deserialize(src)).collect()
+    }
+}
+
+impl Deserialize for String {
+    fn deserialize(src: &mut Cursor<Vec<u8>>) -> Result<Self, Error> {
+        let buf = Vec::<u8>::deserialize(src)?;
+        Ok(String::from_utf8(buf)?)
     }
 }
--- src/serialize.rs
+++ src/serialize.rs
@@ -5,6 +5,12 @@ pub trait Serialize {
     fn serialize(&self, buf: &mut Vec<u8>);
 }
 
+impl Serialize for u8 {
+    fn serialize(&self, buf: &mut Vec<u8>) {
+        buf.extend(self.to_le_bytes());
+    }
+}
+
 impl Serialize for i64 {
     fn serialize(&self, buf: &mut Vec<u8>) {
         buf.extend(self.to_le_bytes());
@@ -26,5 +32,18 @@ impl Serialize for Uuid {
 impl Serialize for Decimal {
     fn serialize(&self, buf: &mut Vec<u8>) {
         buf.extend(self.serialize());
+    }
+}
+
+impl<T: Serialize> Serialize for [T] {
+    fn serialize(&self, buf: &mut Vec<u8>) {
+        (buf.len() as u8).serialize(buf);
+        self.iter().for_each(|e| e.serialize(buf));
+    }
+}
+
+impl Serialize for str {
+    fn serialize(&self, buf: &mut Vec<u8>) {
+        self.as_bytes().serialize(buf);
     }
 }
